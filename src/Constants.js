cc16c9c47a4f18ce5027b0f7dd918ad1be08fa86
kumarpramodlohar <kumarpramod.asn@gmail.com>
2023-12-21 10:03:15 +0530

    initial commit

--- src/Constants.js
+++ src/Constants.js
@@ -1,7 +1,9 @@
-export default data = [
+const data = [
   'https://images.unsplash.com/photo-1542397284385-6010376c5337?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D',
   'https://images.unsplash.com/photo-1490077476659-095159692ab5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTB8fHxlbnwwfHx8fHw%3D',
   'https://media.istockphoto.com/id/1662909784/photo/american-football-arena-with-yellow-goal-post-grass-field-and-blurred-fans-at-playground-view.webp?b=1&s=170667a&w=0&k=20&c=hqYfWF03GFzy58AhkTIOvQR7zjWT0ug7KsuVvd_qw1Y=',
   'https://images.unsplash.com/photo-1542641734-3b824eaabad0?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTh8fHxlbnwwfHx8fHw%3D',
   'https://images.unsplash.com/photo-1542451313056-b7c8e626645f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D',
 ];
+
+export {data};
--- src/ImageSlider.jsx
+++ src/ImageSlider.jsx
@@ -1,3 +1,19 @@
+import { useState } from 'react';
+import {data} from './Constants';
+
 export default function ImageSlider() {
-  return <div> Hello Image</div>;
+  const [activeImageIndex,setActiveImageIndex] =useState(0);
+  return (
+  <div className='flex justify-center'> 
+  <button className='font-bold p4 m-10'>Previous</button>
+  {data.map((url,i)=>{
+    <img 
+    key={url}
+    src={url} 
+    className={"w-[700px] h-[500px] object-contain " + (activeImageIndex === i ? "block" : "hidden")}
+    alt='wallpaper'/>
+  })}
+  <button className='font-bold p-4 m-10'>Next</button>
+  </div>
+  );
 }
